c5b19acec5785e8c43575a439f639b2266d5a9cf
GilesJ <gilesjeremydev@gmail.com>
2020-06-07 21:38:16 +0200

    P.1.1 & P.1.2

---
+++ Unit-1-Build-your-first-Android-app-in-Kotlin/P.1.1_Write-your-first-program-in-Kotlin/main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Happy Birthday!")
+    println("Jhansi")
+    println("You are 25!")
+
+    // or
+    print("Happy Birthday!\n")
+    print("Jhansi\n")
+    print("You are 25!")
+}
---
+++ Unit-1-Build-your-first-Android-app-in-Kotlin/P.1.2_Create-a-birthday-message-in-Kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val age = 30 * 365
+    val name = "Jérémy"
+
+    println("Happy Birthday, ${name}!")
+
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+
+    println("")
+
+    println("You are already ${age} days old, ${name}!")
+    println("${age} days old is the very best age to celebrate!")
+}
