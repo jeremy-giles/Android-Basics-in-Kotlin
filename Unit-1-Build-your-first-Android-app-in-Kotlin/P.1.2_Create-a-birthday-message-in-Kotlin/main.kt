8ea5129e5eb7b3f37620eb28ee4f717df6da9a23
GilesJ <gilesjeremydev@gmail.com>
2020-06-08 21:15:42 +0200

    P.1.2

--- Unit-1-Build-your-first-Android-app-in-Kotlin/P.1.2_Create-a-birthday-message-in-Kotlin/main.kt
+++ Unit-1-Build-your-first-Android-app-in-Kotlin/P.1.2_Create-a-birthday-message-in-Kotlin/main.kt
@@ -1,5 +1,6 @@
 fun main() {
-    val age = 30 * 365
+    val age = 30
+    val days = age * 365
     val name = "Jérémy"
 
     println("Happy Birthday, ${name}!")
@@ -13,6 +14,54 @@ fun main() {
 
     println("")
 
-    println("You are already ${age} days old, ${name}!")
-    println("${age} days old is the very best age to celebrate!")
+    println("You are already ${days} days old, ${name}!")
+    println("${days} days old is the very best age to celebrate!")
+
+    val border = "`-._,-'"
+    val timesToRepeat = 4
+    printBorder(border, timesToRepeat)
+    println("  Happy Birthday, Jérémy!")
+    printBorder(border, timesToRepeat)
+
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+}
+
+fun printBorder(border: String, timesToRepeat: Int) {
+    repeat(timesToRepeat) {
+        print(border)
+    }
+    println()
+}
+
+fun printCakeCandles(age: Int) {
+    print (" ")
+    repeat(age) {
+        print(",")
+    }
+    println() // Print an empty line
+
+    print(" ") // Print the inset of the candles on the cake
+    repeat(age) {
+        print("|")
+    }
+    println()
+}
+
+fun printCakeTop(age: Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers) {
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
 }
